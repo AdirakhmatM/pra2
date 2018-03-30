99206ba420879d1c9304a86de1f51f068e74b41f
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-03-30 12:19:37 +0800

    Fill Nilai Attribut

--- method/src/pra2/SoundspeakAct.java
+++ method/src/pra2/SoundspeakAct.java
@@ -3,5 +3,7 @@ package pra2;
 public class SoundspeakAct {
     public static void main(String[] args) {
         Soundspeak s1 = new Soundspeak();
+        s1.Merk="Max Bass";
+        s1.Harga= 700;
     }
 }
