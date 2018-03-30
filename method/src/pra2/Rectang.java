6b7ef8bf227a87d01073e9ba891660db35dd5bff
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-03-30 12:02:03 +0800

    Create Attribut dan Behavior

--- method/src/pra2/Rectang.java
+++ method/src/pra2/Rectang.java
@@ -1,5 +1,13 @@
 package pra2;
 
 public class Rectang {
-    
+   String Merk;
+   int Harga;
+   
+   void listensound(){
+       System.out.println("+----------------");
+       System.out.println("| Merk = "+Merk);
+       System.out.println("| Harga = "+Harga);
+       System.out.println("+----------------");
+   }
 }
